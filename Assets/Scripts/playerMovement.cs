24d1f05bed42c2b13e5b55f6dc30dd92eac42ee6
Evans Chen <evansschen@gmail.com>
2020-06-24 22:12:48 -0500

    Add double jump

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -31,12 +31,21 @@ public class playerMovement : MonoBehaviour
             float x = Mathf.MoveTowards(rb.velocity.x, 0f, 40f * Time.deltaTime);
             rb.velocity = new Vector2(x, rb.velocity.y);
         }
+        // ground behavior
         if (IsGrounded())
         {
-            numberOfJumps = 2;
+            if (rb.velocity.y < 55f * 0.1f)
+            {
+                numberOfJumps = 1;
+            }
         }
-        if (input.MoveY > 0)
+        // jump
+        if (input.MoveY > 0 && rb.velocity.y < 55f * 0.2f)
         {
+            if (IsGrounded())
+            {
+                numberOfJumps = 2;
+            }
             if (numberOfJumps > 0)
             {
                 numberOfJumps--;
@@ -48,7 +57,7 @@ public class playerMovement : MonoBehaviour
     RaycastHit2D[] hits = new RaycastHit2D[10];
     public bool IsGrounded()
     {
-        int count = Physics2D.RaycastNonAlloc(transform.position, Vector3.down, hits, 5f);
+        int count = Physics2D.RaycastNonAlloc(transform.position + 3.9f * Vector3.down, Vector3.down, hits, 0.2f);
         for (int i = 0; i < count; i++)
         {
             if (hits[i].collider.gameObject.tag == "Ground")
