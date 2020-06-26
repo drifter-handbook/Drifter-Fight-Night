255020424f36bc7e27b2d926c295a063341b462f
Lyn Worzalla <worzalladev@gmail.com>
2020-06-26 10:23:06 -0700

    Add control flow

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -6,8 +6,19 @@ using UnityEngine;
 /**
  * This deals with all UI inputs
  */
+ [DisallowMultipleComponent]
 public class UIController : MonoBehaviour
 {
+
+    public enum Views {
+        DEFAULT,            // Fall-back state, should never happen
+        NONE,               // Don't show UI
+        MENU,               // Viewing in-game menu
+        OPTIONS,            // Adjusting game options
+        CHARACTER_SELECT,   // Lobby
+        PAUSED,             // Pause menu
+        POST_MATCH,         // Show summary
+    }
     // Start is called before the first frame update
     void Start()
     {
