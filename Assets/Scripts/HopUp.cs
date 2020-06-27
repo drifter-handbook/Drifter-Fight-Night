0f8613d732c17a4b18c0706b480269fde2559b19
Lyn Worzalla <worzalladev@gmail.com>
2020-06-26 20:58:29 -0700

    Modify controls

---
+++ Assets/Scripts/HopUp.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HopUp : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.position = new Vector3(transform.position.x, 
+                transform.position.y + 4.0f, 1);
+
+            
+            other.rigidbody.velocity = Vector3.zero;
+        }
+    }
+}
