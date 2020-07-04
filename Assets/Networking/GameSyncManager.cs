2123ee2cf4232cca7382150277d62ed194465ac0
Lyn Worzalla <worzalladev@gmail.com>
2020-07-03 18:45:36 -0700

    Clean up and fix merge

--- Assets/Networking/GameSyncManager.cs
+++ Assets/Networking/GameSyncManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerInput))]
 public class GameSyncManager : MonoBehaviour
 {
     NetworkHost host;
--- Assets/Scripts/Character/PlayerInput.cs
+++ Assets/Scripts/Character/PlayerInput.cs
@@ -8,11 +8,6 @@ public class PlayerInput : MonoBehaviour
     public PlayerInputData input;
     public CustomControls keyBindings;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {
