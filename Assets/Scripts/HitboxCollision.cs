b2c5924382e9459ddf3127fa444a697f99070e55
Richitt <richitt10@gmail.com>
2020-07-12 01:59:40 -0400

    starting collision logic

---
+++ Assets/Scripts/HitboxCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxCollision : MonoBehaviour
+{
+    public GameObject parent;
+    // Start is called before the first frame update
+    CapsuleCollider2D capsule;
+    void Start()
+    {
+      capsule = GetComponentInChildren<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+       Debug.Log("hi");
+     }
+}
