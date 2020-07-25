6162c4bfb7d873b10bcc5b743d098b37db3b3486
Richitt <richitt10@gmail.com>
2020-07-25 01:46:08 -0400

    adding template and example for animator script

---
+++ Assets/IMasterHit.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+public interface IMasterHit
+{
+    void callTheMovementEffect();
+    void callTheAttackEffect();
+}
---
+++ Assets/MasterHit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterHit : MonoBehaviour, IMasterHit
+{
+    PlayerAttacking attacking;
+    PlayerMovement playerMovement;
+    public GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attacking = parent.GetComponent<PlayerAttacking>();
+        playerMovement = parent.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attacking = parent.GetComponent<PlayerAttacking>();
+        playerMovement = parent.GetComponent<PlayerMovement>();
+    }
+    void callTheAttackEffect(){
+        //call the Attack here (using the parent)
+    }
+    void callTheMovementEffect(){
+        //call the movement caused by the attack here (in order to force move)
+    }
+}
