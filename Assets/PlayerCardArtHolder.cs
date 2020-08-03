3f1ccf848a4d90733d17987a690c769292e297cf
Savannah Mann <mannnnnn@users.noreply.github.com>
2020-08-02 23:32:40 -0500

    Player Summaries for Combat
    The base of some skeleton code. Not the real skeleton code, we're talking like... idk, 30% of a ribcage

---
+++ Assets/PlayerCardArtHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCardArtHolder : MonoBehaviour
+{
+    public Sprite[] faces = new Sprite[8];
+    public Sprite[] stocks = new Sprite[8];
+}
