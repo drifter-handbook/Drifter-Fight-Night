22c67c6f01ecfd41cea0c2eb63b8b0f6f32adcb0
NoahKliemann <Packerfan504@gmail.com>
2020-08-02 20:34:39 -0500

    Updated MoosejawArena
    Moved around things on the moosejaw arena for spacing, map is now in battlfield configuration. Also the bottom of the stage now has Collision

--- Assets/Scripts/World/HopUp.cs
+++ Assets/Scripts/World/HopUp.cs
@@ -4,13 +4,15 @@ public class HopUp : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other)
     {
+        UnityEngine.Debug.Log("Collision");
         if (other.gameObject.tag == "Player")
         {
             other.transform.position = new Vector3(transform.position.x,
-                transform.position.y + 4.0f, 1);
+                transform.position.y + 8.0f, 1);
 
 
             other.rigidbody.velocity = Vector3.zero;
+            UnityEngine.Debug.Log("Warped");
         }
     }
 }
