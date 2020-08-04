45b3494655299af79c9948d9e9276ca31582e429
Savannah Mann <mannnnnn@users.noreply.github.com>
2020-08-03 22:01:57 -0500

    Player Summary Cards for Combat
    Bottom Panel prefab now looks for Drifters when waking up, sets up their cards, and keeps their cards updated

--- Assets/PlayerCard.cs
+++ Assets/PlayerCard.cs
@@ -6,9 +6,71 @@ using UnityEngine.UI;
 public class PlayerCard : MonoBehaviour
 {
     public GameObject face;
-    public GameObject stock;
+    public Sprite stockImage;
     public Text percent;
     public int currStocks = 0;
 
+    const int MAX_STOCKS = 4;
+
+
     public GameObject stockHolder;
+
+    public void addStock(GameObject stock)
+    {
+        addStocks(stock, 1);
+    }
+    public void addStocks(GameObject stock, int num)
+    {
+        if (currStocks + num > MAX_STOCKS)
+        {
+            num = MAX_STOCKS - currStocks;
+        }
+
+        for(int i = 1; i< MAX_STOCKS; i++)
+        {
+            GameObject newStock = Instantiate(stock, new Vector3(0,0), Quaternion.identity);
+            newStock.transform.parent = stockHolder.transform;
+            newStock.transform.localScale = new Vector3(1, 1, 1);
+            newStock.GetComponent<Image>().sprite = stockImage;
+            currStocks++;
+        }
+    }
+
+    public void removeStock()
+    {
+        removeStocks(1);
+    }
+
+    public void removeStocks(int num)
+    {
+        if(currStocks <= 0)
+        {
+            return;
+        }
+
+        if (stockHolder.transform.childCount < num)
+        {
+            num = stockHolder.transform.childCount;
+        }
+
+        for(int i = 0; i<num; i++)
+        {
+            Destroy(stockHolder.transform.GetChild(0));
+        }
+
+    }
+
+
+    public void setImages(Sprite face, Sprite stock)
+    {
+        this.face.GetComponent<Image>().sprite = face;
+        this.stockImage = stock;
+    }
+
+    public void setPercent(float sentPercent)
+    {
+        this.percent.text = (int)sentPercent+"%";
+    }
+
+
 }
--- Assets/PlayerCardArtHolder.cs
+++ Assets/PlayerCardArtHolder.cs
@@ -6,4 +6,56 @@ public class PlayerCardArtHolder : MonoBehaviour
 {
     public Sprite[] faces = new Sprite[8];
     public Sprite[] stocks = new Sprite[8];
+    public GameObject summaryCardPrefab;
+    public GameObject stockPrefab;
+
+    private Drifter[] drifters;
+    private PlayerCard[] playerCards;
+
+    void Awake()
+    {
+        drifters = FindObjectsOfType<Drifter>();
+
+        playerCards = new PlayerCard[drifters.Length];
+
+        int i = 0; //i know i know i just like foreach ok
+        Debug.Log(drifters.Length);
+        foreach(Drifter drifter in drifters)
+        {
+           GameObject newCard = Instantiate(summaryCardPrefab, transform.position, transform.rotation);
+            newCard.transform.parent = gameObject.transform;
+            newCard.transform.localScale = new Vector3(1, 1, 1);
+            playerCards[i] = newCard.GetComponent<PlayerCard>();
+            int imageIndex = getDrifterTypeIndex(drifter.drifterData);
+            playerCards[i].setImages(faces[imageIndex], stocks[imageIndex]);
+            playerCards[i].addStocks(stockPrefab,3);
+            i++;
+        }
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i< drifters.Length; i++)
+        {
+            playerCards[i].setPercent(drifters[i].DamageTaken);
+        }
+    }
+
+
+    private int getDrifterTypeIndex(DrifterData data)
+    {
+        switch (data.ReadableName)
+        {
+            case ("Bojo"): return 0;
+            case ("Swordfrog"): return 1;
+            case ("Lady Parhelion"): return 2;
+            case ("Spacejam"): return 3;
+            case ("Orro"): return 4;
+            case ("Ryyke"): return 5;
+            case ("Megurin"): return 6;
+            case ("Nero"): return 7;
+            default: return 7;
+        }
+    }
+
 }
