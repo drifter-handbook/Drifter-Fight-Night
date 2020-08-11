d6870bcafde1f7e0842d148c05d7e26793d80039
Savannah Mann <mannnnnn@users.noreply.github.com>
2020-08-10 23:45:42 -0500

    Music Added!
    Character select and stages now have music

---
+++ Assets/Jukebox.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jukebox : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] soundtrack;
+    [Range(0, 1)]
+    public float volume = 0.5f;
+
+    private Random rand;
+    private int selectedSong = -1;
+    private float lastVolume = 0;
+
+
+    void Initialize(int seed)
+    {
+        Random.InitState(seed);
+    }
+
+    void InitializeWithSong(int index)
+    {
+        selectedSong = index;
+    }
+
+    private void Update()
+    {
+        if (lastVolume != volume)
+        {
+            audioSource.volume = volume;
+        }
+    }
+
+    void Start()
+    {
+        //pick your sound!
+        if(selectedSong < 0)
+        {
+            selectedSong = (int)(Random.value*(soundtrack.Length-1));
+        }
+        audioSource.clip = soundtrack[selectedSong];
+        audioSource.Play();
+    }
+
+    
+}
