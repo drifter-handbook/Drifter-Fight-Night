b489e10cc881222977eac10ced2b53e81ec1b454
AustinBrandt <AustinBrandtSoftware@gmail.com>
2020-08-15 18:52:20 -0500

    Tombstone now spawns zombies correctly

---
+++ Assets/Scripts/Character/RyykeUppercutZombie.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RyykeUppercutZombie : MonoBehaviour
+{
+    //public HitboxCollision hitbox;
+    Rigidbody2D rb;
+    //Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	rb = GetComponent<Rigidbody2D>();
+    	rb.velocity= new Vector2(0f,30f);
+        StartCoroutine(Delete());
+    }
+
+    public IEnumerator Delete()
+    {
+        yield return new WaitForSeconds(0.6f);
+        Destroy(gameObject);
+        yield break;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
