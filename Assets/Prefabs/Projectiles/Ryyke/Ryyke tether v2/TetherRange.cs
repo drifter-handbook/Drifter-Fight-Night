a6dfff69298e09fc37c006c91deb02e5b1352ef5
AustinBrandt <AustinBrandtSoftware@gmail.com>
2020-08-17 00:13:15 -0500

    Ryyke Tether and stage cleanup

---
+++ Assets/Prefabs/Projectiles/Ryyke/Ryyke tether v2/TetherRange.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetherRange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    public Vector3 TetherPoint;
+    public string tag;
+
+
+    void Start()
+    {
+
+    }
+
+
+    IEnumerator Delete(){
+        yield return new WaitForSeconds(.5f);
+        Destroy(gameObject);
+        yield break;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == tag)
+        {
+            TetherPoint = col.gameObject.transform.position;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+        if(col.gameObject.tag == tag)
+        {
+            TetherPoint = Vector3.zero;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
