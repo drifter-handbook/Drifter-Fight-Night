fdf1e28febf8063c292eb3886d64498a246d0238
AustinBrandt <AustinBrandtSoftware@gmail.com>
2020-09-01 13:03:00 -0500

    Added endgame banner & modified round start
    To keep more inline with the art style

--- Assets/Scripts/World/KillBox.cs
+++ Assets/Scripts/World/KillBox.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class KillBox : MonoBehaviour    //TODO: Refactored, needs verification
 {
     NetworkEntityList Entities;
+    public Animator endgameBanner;
 
     void Awake()
     {
@@ -84,6 +85,9 @@ public class KillBox : MonoBehaviour    //TODO: Refactored, needs verification
                 if (count != 1)
                 {
                     GameController.Instance.winner = null;
+                }
+                else{
+                    endgameBanner.enabled = true;
                 }
             }
         }
