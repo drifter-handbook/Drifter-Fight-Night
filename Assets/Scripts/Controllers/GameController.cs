83a77eb1983d240133eb194be2e0bb4bcad57b7a
Evans Chen <evansschen@gmail.com>
2020-08-31 23:07:34 -0500

    Add ability to leave

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -138,5 +138,9 @@ public class GameController : MonoBehaviour
         {
             IsPaused = !IsPaused;
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
