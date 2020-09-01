35158d0edd41c045ffe1fdb17bde6b04a8ca8254
AustinBrandt <AustinBrandtSoftware@gmail.com>
2020-08-31 20:02:56 -0500

    Kill should work

--- Assets/Scripts/Character/PlayerStatus.cs
+++ Assets/Scripts/Character/PlayerStatus.cs
@@ -64,7 +64,7 @@ public class PlayerStatus : MonoBehaviour
 
     public bool HasRemovableEffect()
     {
-        return HasStatusEffect(PlayerStatusEffect.END_LAG) || HasStatusEffect(PlayerStatusEffect.PLANTED) || HasStatusEffect(PlayerStatusEffect.STUNNED)||  HasStatusEffect(PlayerStatusEffect.EXPOSED) || HasStatusEffect(PlayerStatusEffect.REVERSED);
+        return HasStatusEffect(PlayerStatusEffect.PLANTED) || HasStatusEffect(PlayerStatusEffect.STUNNED)||  HasStatusEffect(PlayerStatusEffect.EXPOSED) || HasStatusEffect(PlayerStatusEffect.REVERSED);
     }
     public bool HasEnemyStunEffect()
     {
