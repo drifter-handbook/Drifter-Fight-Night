f71f95f0d4efc3f11cc41165310982a3b1f7544a
Pascal King <pmkgbwis@gmail.com>
2020-09-01 00:06:40 -0500

    The announcer does some announcing i guess

---
+++ Assets/Scripts/Audio/SingleSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleSound : MonoBehaviour
+{
+    [SerializeField] public AudioClip Clip;
+
+
+    public void PlayAudio()
+    {
+        AudioSource source = GameController.Instance.GetComponent<AudioSource>();
+        source.clip = Clip;
+        source.PlayOneShot(source.clip);
+    }
+}
