df2cd49ce830cce7f378428fb1de8bfdfa342e28
AustinBrandt <AustinBrandtSoftware@gmail.com>
2020-09-01 23:40:09 -0500

    Bojus Jojus

--- Assets/Scripts/Audio/MultiSound.cs
+++ Assets/Scripts/Audio/MultiSound.cs
@@ -6,17 +6,17 @@ public class MultiSound : MonoBehaviour
 {
     [SerializeField] public AudioClip[] Clips;
 
+    public AudioSource source;
+
 
     public void PlayAudio()
     {
-        AudioSource source = GameController.Instance.GetComponent<AudioSource>();
         source.clip = Clips[UnityEngine.Random.Range(0, Clips.Length)];
         source.PlayOneShot(source.clip);
     }
 
     public void PlayAudio(int index)
     {
-        AudioSource source = GameController.Instance.GetComponent<AudioSource>();
         source.clip = Clips[index];
         source.PlayOneShot(source.clip);
     }
