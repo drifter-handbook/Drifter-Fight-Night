25ae75b0fe78d125a71b0f49eedbf51f7552836a
NoahKliemann <Packerfan504@gmail.com>
2020-10-15 22:17:59 -0500

    Host IP visible on Character Select Screen
    Added Host IP to bottom of character select screen

---
+++ Assets/Scripts/UI/PlayerIP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class PlayerIP : MonoBehaviour
+{
+
+    public Text hostIP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            string holepunch_ip = Resources.Load<TextAsset>("Config/server_ip").text.Trim();
+            hostIP.text = $"{GameController.Instance.GetComponent<IPWebRequest>().result.ToString()}:{UDPHolePuncher.GetLocalIP(holepunch_ip, 6970).GetAddressBytes()[3]}";
+    }
+}
