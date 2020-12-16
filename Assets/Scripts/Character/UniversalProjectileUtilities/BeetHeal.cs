4853869e6f2349105f4d9c98321383f9333da6da
AustinBrandt <AustinBrandtSoftware@gmail.com>
2020-12-16 16:35:56 -0600

    Beets by mytharisu 2.0

---
+++ Assets/Scripts/Character/UniversalProjectileUtilities/BeetHeal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeetHeal : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(!GameController.Instance.IsHost)return;
+
+		float damage = collider.gameObject.GetComponent<HurtboxCollision>().parent.GetComponent<Drifter>().DamageTaken;
+
+		collider.gameObject.GetComponent<HurtboxCollision>().parent.GetComponent<Drifter>().DamageTaken = Mathf.Max(0f,damage - 6f);
+
+		for(int i = 0; i < 5;i++)
+    	{
+    		GraphicalEffectManager.Instance.CreateMovementParticle(MovementParticleMode.Heal, collider.transform.position + new Vector3(UnityEngine.Random.Range(2f,-2f), UnityEngine.Random.Range(0f,5f)), 0, new Vector2(1, 1));
+    	}
+
+    	Destroy(gameObject.transform.parent.gameObject);
+
+	}
+}
