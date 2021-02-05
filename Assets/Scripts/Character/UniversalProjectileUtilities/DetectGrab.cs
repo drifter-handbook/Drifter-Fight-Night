95829de35bb9e20bc1df61a912e8bcde234fa1f9
AustinBrandt <AustinBrandtSoftware@gmail.com>
2021-02-05 00:14:22 -0600

    Update DetectGrab.cs

--- Assets/Scripts/Character/UniversalProjectileUtilities/DetectGrab.cs
+++ Assets/Scripts/Character/UniversalProjectileUtilities/DetectGrab.cs
@@ -21,7 +21,7 @@ public class DetectGrab : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         if(!GameController.Instance.IsHost)return;
-        if(col.gameObject.name == "Hurtboxes"
+        if(col.gameObject.layer == 10
          && col.gameObject.GetComponent<HurtboxCollision>().parent.GetComponent<Drifter>() != drifter
          && !col.GetComponent<HurtboxCollision>().parent.GetComponent<PlayerStatus>().HasStatusEffect(PlayerStatusEffect.INVULN)
          && col.gameObject.GetComponent<HurtboxCollision>().parent.GetComponent<PlayerHurtboxHandler>().CanHit(gameObject.GetComponent<HitboxCollision>().AttackID))
