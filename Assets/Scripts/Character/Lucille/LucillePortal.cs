fbebf0788fc4006673434c0aefbbdc54fb635057
AustinBrandt <AustinBrandtSoftware@gmail.com>
2021-02-08 19:40:18 -0600

    Lucille's rift merging functional

--- Assets/Scripts/Character/Lucille/LucillePortal.cs
+++ Assets/Scripts/Character/Lucille/LucillePortal.cs
@@ -13,38 +13,15 @@ public class LucillePortal : MonoBehaviour
 
 	public float myPriority;
 	public SyncAnimatorStateHost anim;
+	bool canMerge  = true;
+	Rigidbody2D rb;
 
 	void Start()
 	{
+		rb = GetComponent<Rigidbody2D>();
 		anim = GetComponent<SyncAnimatorStateHost>();
 		myPriority = size;
 	}
-
-
-	// void OnTriggerEnter2D(Collider2D collider)
-	// {
-	// 	if(!GameController.Instance.IsHost)return;
-
-	// 	UnityEngine.Debug.Log("HIT");
-
-	// 	if(collider.gameObject.tag == "Lucille_Portal")
-	// 	{
-	// 		LucillePortal merging_Portal = collider.GetComponent<LucillePortal>();
-
-	// 		if(merging_Portal.drifter == drifter)
-	// 		{
-	// 			if(merging_Portal.HandleMerge(myPriority))
-	// 			{
-	// 				size += 1;
-	// 				anim.SetState("Rift_" + size);
-
-	// 			}
-
-	// 		}
-	// 		else anim.SetState("Rift_Decay_" + size); 
-	// 	}
-	// }
-
 
 	void OnTriggerEnter2D(Collider2D collider)
 	{
@@ -52,42 +29,26 @@ public class LucillePortal : MonoBehaviour
 
 		HitboxCollision hitbox = collider.gameObject.GetComponent<HitboxCollision>();
 
-		if(collider.gameObject.tag == "Lucille_Portal")
+		if(collider.gameObject.tag == "Lucille_Portal" && collider.GetComponent<LucillePortal>().drifter == drifter)
 		{
+			canMerge = true;
 			LucillePortal merging_Portal = collider.GetComponent<LucillePortal>();
 
-			if(merging_Portal.drifter == drifter)
-			{
-				myPriority = size + UnityEngine.Random.Range(0f, 100f) / 100f;
+			myPriority +=size + UnityEngine.Random.Range(0f,.100f);
 
-				if(myPriority > merging_Portal.myPriority && merging_Portal.size !=3)
-				{
-					
-					size++;
-					if(size >3) size=3;
+			merging_Portal.contest(myPriority,this);
 
-					anim.SetState("Rift_" + size);
-
-				}
-				else
-				{
-					anim.SetState("Rift_Decay_" + size);
-					drifter.GetComponentInChildren<LucilleMasterHit>().breakRift(this.gameObject);
-
-				} 
-
-			}
-			
 		}
-
 
 		try
 		{
 			if(hitbox != null && hitbox.parent == drifter && collider.gameObject.tag == "Lucille_Portal_Contact" )
 			{
-				GetComponent<Rigidbody2D>().velocity = new Vector3(hitbox.Facing * ((hitbox.OverrideData.AngleOfImpact < 45f && hitbox.OverrideData.AngleOfImpact > -30f)?35f:0f),(hitbox.OverrideData.AngleOfImpact > 45f ?35f:(hitbox.OverrideData.AngleOfImpact > 20?0:-35f )),0);
+				rb.velocity = new Vector3(hitbox.Facing * ((hitbox.OverrideData.AngleOfImpact < 45f && hitbox.OverrideData.AngleOfImpact > -30f)?35f:0f),(hitbox.OverrideData.AngleOfImpact > 45f ?35f:(hitbox.OverrideData.AngleOfImpact > 20?0:-35f )),0);
 
 				float moveDirection = hitbox.OverrideData.AngleOfImpact;
+
+				myPriority = -3;
 
 				// if((moveDirection > 45f && moveDirection < 135f))anim.SetState("Move_up");
 				// else if(hitbox.Facing > 0)anim.SetState("Move_Right");
@@ -104,7 +65,7 @@ public class LucillePortal : MonoBehaviour
 
 			else if(hitbox != null && hitbox.parent == drifter && collider.gameObject.tag == "Lucille_Portal_Detonate")
 			{
-				drifter.GetComponentInChildren<LucilleMasterHit>().breakRift(this.gameObject);
+				drifter.GetComponentInChildren<LucilleMasterHit>().breakRift(gameObject);
 				detonate();
 				GraphicalEffectManager.Instance.CreateHitSparks(HitSpark.LUCILLE,  Vector3.Lerp(transform.position, hitbox.parent.transform.position, 0.1f), 0, new Vector2(6f, 6f));
 			}
@@ -117,15 +78,49 @@ public class LucillePortal : MonoBehaviour
 		}
 	}
 
+	public void contest(float enemyPriority, LucillePortal other)
+	{
+		if(!canMerge)return;
+		other.canMerge = false;
+
+		myPriority += size + UnityEngine.Random.Range(0f,.100f);
+
+
+		if(myPriority < enemyPriority)
+		{
+			other.grow(size);
+			decay();
+			drifter.GetComponentInChildren<LucilleMasterHit>().breakRift(gameObject);
+		}
+		else 
+		{
+			grow(other.size);
+			other.decay();
+			drifter.GetComponentInChildren<LucilleMasterHit>().breakRift(other.gameObject);
+		}
+		
+	}
+
+	public void grow(int growthIncrement)
+	{
+		UnityEngine.Debug.Log(size + " " +  growthIncrement);
+		size += growthIncrement;
+		//if(size > 3)size = 3;
+		UnityEngine.Debug.Log(size);
+		anim.SetState("Rift_" + size);
+	}
+
 	public void detonate()
 	{
 		if(!GameController.Instance.IsHost)return;
+		rb.velocity = Vector2.zero;
 		anim.SetState("Rift_Detonate_" + size);
 	}
 
 	public void decay()
 	{
 		if(!GameController.Instance.IsHost)return;
+		rb.velocity = Vector2.zero;
 		anim.SetState("Rift_Decay_" + size);
 	}
 }
