f0d62d2d2a6d49f395f5ee2d44987d8d1da3fb1f
pascal <pmkgbwis@gmail.com>
2021-07-21 23:17:56 -0500

    Property Drawer? I barely k

--- Assets/Scripts/Audio/AudioLibrary.cs
+++ Assets/Scripts/Audio/AudioLibrary.cs
@@ -2,13 +2,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "AudioLibrary", menuName = "VirtuaDrifter/AudioLibrary", order = 100)]
 public class AudioLibrary : ScriptableObject
 {
+
+
     [Serializable] public struct StringClipPair {
         public string name;
         public AudioClip clip;
+    }
+
+    // IngredientDrawer
+    [CustomPropertyDrawer(typeof(StringClipPair))]
+    public class StringClipDrawer : PropertyDrawer
+    {
+        // Draw the property inside the given rect
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            // Calculate rects
+            var nameRect = new Rect(position.x + 0, position.y, position.width - 120, position.height);
+            var clipRect = new Rect(position.x + position.width - 120, position.y, 120, position.height);
+
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.PropertyField(nameRect, property.FindPropertyRelative("name"), GUIContent.none);
+            EditorGUI.PropertyField(clipRect, property.FindPropertyRelative("clip"), GUIContent.none);
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
     }
 
     [SerializeField] private StringClipPair[] library;
