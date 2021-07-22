0022994f2e981995795c47d2d1ace2fc7963c437
pascal <pmkgbwis@gmail.com>
2021-07-21 23:01:32 -0500

    i n t o   t h e   s o u p

---
+++ Assets/Scripts/Audio/AudioLibrary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioLibrary", menuName = "VirtuaDrifter/AudioLibrary", order = 100)]
+public class AudioLibrary : ScriptableObject
+{
+    [Serializable] public struct StringClipPair {
+        public string name;
+        public AudioClip clip;
+    }
+
+    [SerializeField] private StringClipPair[] library;
+}
+
+
+
