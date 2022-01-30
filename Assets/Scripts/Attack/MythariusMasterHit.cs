4c49fb96807183e82cf88f0f50b91ee9d611fee4
AustinBrandt <AustinBrandtSoftware@gmail.com>
2022-01-29 21:21:56 -0600

    Mytharius Birb

--- Assets/Scripts/Attack/MythariusMasterHit.cs
+++ Assets/Scripts/Attack/MythariusMasterHit.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MythariusMasterHit : MasterHit
 {
 
+    GameObject bird;
+
     public void warpStart()
     {
         if(!isHost)return;
@@ -24,5 +26,30 @@ public class MythariusMasterHit : MasterHit
         facing = movement.Facing;
         GameObject ring = GameController.Instance.host.CreateNetworkObject("LaunchRing", transform.position + new Vector3(0,1.4f),  transform.rotation);
     }
+
+    public void Spawn_Bird()
+    {
+        if(!isHost)return;
+
+        if(bird != null)
+        {
+            Destroy(bird);
+        }
+        facing = movement.Facing;
+        bird = host.CreateNetworkObject("Mytharius_Bird", transform.position + new Vector3(facing * 1.4f,2.4f), transform.rotation);
+        bird.transform.localScale = new Vector3(10f * facing, 10f , 1f);
+        foreach (HitboxCollision hitbox in bird.GetComponentsInChildren<HitboxCollision>(true))
+        {
+            hitbox.parent = drifter.gameObject;
+            hitbox.AttackID = attacks.AttackID;
+            hitbox.AttackType = attacks.AttackType;
+            hitbox.Facing = facing;
+       }
+
+       bird.GetComponent<SyncProjectileColorDataHost>().setColor(drifter.GetColor());
+       bird.GetComponent<Bird_Wrangler>().setup(drifter.gameObject,attacks,facing,drifter.GetColor());
+
+    }
+
 
 }
---
+++ Assets/Scripts/Character/Mytharius/Bird_Wrangler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird_Wrangler : MonoBehaviour
+{
+
+	PlayerAttacks attacks;
+    GameObject Mytharius;
+    int facing;
+    int color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void setup(GameObject p_drifter, PlayerAttacks p_attacks, int p_facing,int p_color)
+    {
+    	Mytharius = p_drifter;
+    	attacks = p_attacks;
+    	facing = p_facing;
+    	color = p_color;
+    }
+
+    public void Drop_Letter()
+    {
+        if(!GameController.Instance.IsHost)return;
+
+        GameObject letter = GameController.Instance.host.CreateNetworkObject("Mytharius_Letter", transform.position, transform.rotation);
+        letter.transform.localScale = new Vector3(facing *10,10,1f);
+        attacks.SetMultiHitAttackID();
+        foreach (HitboxCollision hitbox in letter.GetComponentsInChildren<HitboxCollision>(true))
+        {
+            hitbox.parent = Mytharius;
+            hitbox.AttackID = attacks.AttackID;
+            hitbox.AttackType = attacks.AttackType;
+            hitbox.Facing = facing;
+        }
+
+        letter.GetComponent<SyncProjectileColorDataHost>().setColor(color);
+    }
+
+}
