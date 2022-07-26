ae9a7ae8c7ec2802775c6eea96bba149f8505070
AustinBrandt <AustinBrandtSoftware@gmail.com>
2022-07-25 21:26:52 -0500

    Basic Training tree

--- Assets/Scripts/UI/TrainingDummyHandler.cs
+++ Assets/Scripts/UI/TrainingDummyHandler.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class TrainingDummyHandler : MonoBehaviour
 {
 	public Drifter Dummy;
-	public Dropdown m_Dropdown;
+	public Dropdown d_Dropdown;
+    public Dropdown t_Dropdown;
+    public Dropdown r_Dropdown;
 
     //-1    no option
     //0     dash
@@ -14,84 +16,230 @@ public class TrainingDummyHandler : MonoBehaviour
     //2     jab
     //3     random
 
-    int wakeupMode = -1;
+    // int wakeupMode = -1;
 
     bool onHit = false;
+    bool onBlock = false;
+    bool onWakeup = false;
+
+    int reset = 0;
+
+    int resetFrames = 28;
+    bool resetFlag = true;
+
+    PlayerInputData[] baseState     = new PlayerInputData[]
+        {
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData()
+        };
+    PlayerInputData[] reactionState = new PlayerInputData[]
+        {
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData()
+        };
 
 	int option;
 
 	void Start()
     {
         //Add listener for when the value of the Dropdown changes, to take action
-        m_Dropdown.onValueChanged.AddListener(delegate {
-            DropdownValueChanged(m_Dropdown);
+        d_Dropdown.onValueChanged.AddListener(delegate {
+            BaseDropdownValueChanged(d_Dropdown);
+        });
+
+        t_Dropdown.onValueChanged.AddListener(delegate {
+            TriggerDropdownValueChanged(t_Dropdown);
+        });
+
+        r_Dropdown.onValueChanged.AddListener(delegate {
+            ReactionDropdownValueChanged(r_Dropdown);
         });
     }
 
     void FixedUpdate()
     {
 
-        if(wakeupMode >=0 && Dummy.knockedDown)
+        // if(onWakeup && Dummy.knockedDown)
+        // {
+        //     switch(wakeupMode)
+        //     {
+        //         case 0:
+        //             Dummy.input[0] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
+        //             Dummy.input[2] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
+        //             break;
+        //         case 1:
+        //             Dummy.input[0] = new PlayerInputData(){Jump = true};
+        //             break;
+        //         case -1:
+        //         default:
+        //             break;
+        //     }
+        // }
+        // if(onBlock && Dummy.status.HasEnemyStunEffect() && Dummy.guarding)
+        // {
+        //     Dummy.input[0] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
+        //     Dummy.input[2] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
+        // }
+
+
+        // if((Dummy.movement.dashing || Dummy.movement.jumping ))
+        // {
+        //     Dummy.input[0] = new PlayerInputData() {Guard = onHit};
+        //     Dummy.input[1] = new PlayerInputData();
+        //     Dummy.input[2] = new PlayerInputData();
+        // }
+
+        if(onWakeup && Dummy.knockedDown)
         {
-            switch(wakeupMode)
+            if(resetFlag)setDummyInput(reactionState);
+            resetFlag = false;
+            reset = resetFrames;
+        }
+
+        if(onBlock && Dummy.status.HasEnemyStunEffect() && Dummy.guarding)
+        {
+            if(resetFlag) setDummyInput(reactionState);
+            resetFlag = false;
+            reset = resetFrames;
+        }
+
+        if(onHit && Dummy.status.HasEnemyStunEffect())
+        {
+            if(resetFlag) setDummyInput(reactionState);
+            resetFlag = false;
+            reset = resetFrames;
+        }
+
+        if((onHit || onBlock)  && !Dummy.status.HasEnemyStunEffect() && reset <=0 )
+             setDummyInput(baseState);
+
+
+        if(reset > 0)
+        {
+            reset--;
+            if(reset <=0)
             {
-                case 0:
-                    Dummy.input[0] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
-                    Dummy.input[2] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
-                    break;
-                case 1:
-                    Dummy.input[0] = new PlayerInputData(){Jump = true};
-                    break;
-                case -1:
-                default:
-                    break;
+                resetFlag = true;
+                setDummyInput(baseState);
             }
         }
-        if(onHit && Dummy.status.HasEnemyStunEffect() && Dummy.guarding)
-        {
-            Dummy.input[0] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
-            Dummy.input[2] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
-        }
 
-
-        if((wakeupMode >=0 || onHit)&&(Dummy.movement.dashing || Dummy.movement.jumping ))
-        {
-            Dummy.input[0] = new PlayerInputData() {Guard = onHit};
-            Dummy.input[1] = new PlayerInputData();
-            Dummy.input[2] = new PlayerInputData();
-        }
 
     }
 
     //Ouput the new value of the Dropdown into Text
-    void DropdownValueChanged(Dropdown change)
+    void BaseDropdownValueChanged(Dropdown change)
     {
-        wakeupMode = -1;
-        onHit = false;
+        baseState = new PlayerInputData[]
+        {
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData()
+        };
     	switch(change.value)
     	{
-    		case 0:
-    			Dummy.input[0] = new PlayerInputData();
-    			break;
     		case 1:
-    			Dummy.input[0] = new PlayerInputData(){Guard = true};
+                Dummy.input[0] = new PlayerInputData(){Guard = true};
+    			baseState[0] = new PlayerInputData(){Guard = true};
     			break;
     		case 2:
-    			Dummy.input[0] = new PlayerInputData(){Jump = true};
+                Dummy.input[0] = new PlayerInputData(){Jump = true};
+    			baseState[0] = new PlayerInputData(){Jump = true};
     			break;
             case 3:
-                wakeupMode = 0;
+                Dummy.input[0] = new PlayerInputData(){Light = true};
+                baseState[0] = new PlayerInputData(){Light = true};
+                Dummy.input[1] = new PlayerInputData(){Light = true};
+                baseState[1] = new PlayerInputData(){Light = true};
                 break;
             case 4:
-                wakeupMode = 1;
+                Dummy.input[0] = new PlayerInputData(){Special = true};
+                baseState[0] = new PlayerInputData(){Special = true};
+                Dummy.input[1] = new PlayerInputData(){Special = true};
+                baseState[1] = new PlayerInputData(){Special = true};
                 break;
-    		case 5:
-                Dummy.input[0] = new PlayerInputData(){Guard = true};
-                onHit = true;
-    			break;
-            case 6:
+            case 0:
     		default:
     			break;
     	};
+    }
+
+    //Ouput the new value of the Dropdown into Text
+    void TriggerDropdownValueChanged(Dropdown change)
+    {
+        onWakeup = false;
+        onHit = false;
+        onBlock = false;
+        resetFlag = true;
+        switch(change.value)
+        {
+            case 1:
+                onWakeup = true;
+                break;
+            case 2:
+                onHit = true;
+                break;
+            case 3:
+                onBlock = true;
+                break;
+            case 0:
+            default:
+                break;
+        };
+    }
+
+    //Ouput the new value of the Dropdown into Text
+    void ReactionDropdownValueChanged(Dropdown change)
+    {
+        reactionState = new PlayerInputData[]
+        {
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData(),
+            new PlayerInputData()
+        };
+
+        resetFrames = 28;
+        switch(change.value)
+        {
+            case 1:
+                reactionState[0] = new PlayerInputData(){Guard = true};
+                resetFrames = 60;
+                break;
+            case 2:
+                reactionState[0] = new PlayerInputData(){Jump = true};
+                break;
+            case 3:
+                reactionState[0] = new PlayerInputData(){Light = true};
+                reactionState[1] = new PlayerInputData(){Light = true};
+                break;
+
+            case 4:
+                reactionState[0] = new PlayerInputData(){Special = true};
+                reactionState[1] = new PlayerInputData(){Special = true};     
+                break;
+
+            case 5:
+                reactionState[0] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
+                reactionState[2] = new PlayerInputData(){MoveX = Dummy.movement.Facing};
+                break;
+
+            case 0:
+            default:
+                break;
+        };
+    }
+
+    void setDummyInput(PlayerInputData[] p_input)
+    {
+        for(int i = 0; i < p_input.Length; i++)
+        {
+            Dummy.input[i] = p_input[i];
+        }
     }
 }
