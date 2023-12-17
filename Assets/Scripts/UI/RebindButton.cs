86f8794d7d026b08123dfbd17cd2f3c088339243
Cindy Prentice <prentlikes2cookies@gmail.com>
2023-12-17 17:57:57 -0600

    Scroll menu for rebind now (almost) scrolls

--- Assets/Scripts/UI/RebindButton.cs
+++ Assets/Scripts/UI/RebindButton.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class RebindButton : MonoBehaviour {
     [SerializeField]
     public InputActionReference actionReference;
     public Text mappingString;
+    private static Action<InputUser, InputUserChange, InputDevice> userChangeCallback;
+    string bindingName = "";
 
     public void Awake() {
-        UpdateMappingString();
+        userChangeCallback = OnUserChange;
+        InputUser.onChange += userChangeCallback;
+    }
+
+    public void OnDestroy()
+    {
+        InputUser.onChange -= userChangeCallback;
     }
 
     public void RemapButtonClicked()
     {
         actionReference.action.Disable();
-        var rebindOperation = actionReference.action.PerformInteractiveRebinding().Start();
+        var rebindOperation = actionReference.action.PerformInteractiveRebinding().WithBindingGroup(bindingName).Start();
         rebindOperation.OnComplete(operation => {
             actionReference.action.Enable();
-            UpdateMappingString();
         });
     }
 
-    public void UpdateMappingString() {
+    public void UpdateMappingString(string name) {
+        bindingName = name;
         mappingString.text = actionReference.action.GetBindingDisplayString();
+    }
+
+    public void OnUserChange(InputUser user, InputUserChange change, InputDevice device)
+    {
+        if(user.controlScheme.HasValue)
+        {
+            UpdateMappingString(user.controlScheme.Value.name);
+        }
     }
 }
