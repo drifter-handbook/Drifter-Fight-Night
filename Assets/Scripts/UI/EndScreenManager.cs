85b6a4a222f1ef8a6417a5feefba53f5597408a3
AustinBrandt <AustinBrandtSoftware@gmail.com>
2024-03-13 16:11:24 -0500

    Rework end screen to function as a voting system
    Each player now selects what the next screen should be.

--- Assets/Scripts/UI/EndScreenManager.cs
+++ Assets/Scripts/UI/EndScreenManager.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using System.IO;
+
+public enum EndgameVote{
+	EMPTY_SLOT,
+	NO_SELECTION,
+	PLAY_AGAIN,
+	CHARACTER_SELECT,
+	DISCONNECT
+}
 
 public class EndScreenManager : UIMenuManager {
 	
 	public Sprite[] sprites;
 	public GameObject[] winnerSetups;
 	public GameObject playAgainButton;
+
+	public GameObject[] buttons;
+
+	EndgameVote[] EndgameVotes;
 
 	public static EndScreenManager Instance { get; private set; }
 
@@ -20,10 +33,12 @@ public class EndScreenManager : UIMenuManager {
 
 	void Start() {
 
-		//Set input to UI action map (was at gameplay action map until reaching this screen) and set initial selected game object.
-		GameController.Instance.toggleInputSystem(true);
 		
-		EventSystem.current.SetSelectedGameObject(GameObject.Find("MainMenu"));
+		if(EndgameVotes == null) SetUpVote();
+		//Set input to UI action map (was at gameplay action map until reaching this screen) and set initial selected game object.
+		//GameController.Instance.toggleInputSystem(true);
+		
+		//EventSystem.current.SetSelectedGameObject(GameObject.Find("MainMenu"));
 
 		//NOTE: used to have some isHost and sendNetworkMessage logic here (see mouse removal revision for commented out code if necessary)
 
@@ -41,15 +56,71 @@ public class EndScreenManager : UIMenuManager {
 	}
 
 	public void UpdateFrame(PlayerInputData[] inputs) {
-		//TODO
-		//Each player present needs to be able to act in this screen to vote on the next action
 
-		//call to base class.
-		PlayerInput[] playerInputs = FindObjectsOfType<PlayerInput>();
-		foreach (PlayerInput playerInput in playerInputs) {
-			UpdateActivePlayerInputs(playerInput);
+		if(EndgameVotes == null) SetUpVote();
+
+		foreach(CharacterSelectState css in CharacterMenu.charSelStates){
+			if(css == null || css.PeerID > 8) break;
+			if(css.prevInput.MoveX == 0 && inputs[css.PeerID].MoveX != 0){
+				css.x += inputs[css.PeerID].MoveX;
+				if(css.x < 0) css.x = buttons.Length - 1;
+				if(css.x >= buttons.Length) css.x = 0;	
+			}
+			css.Cursor.transform.localPosition = buttons[css.x].transform.position;
+
+			if ((inputs[css.PeerID].Light && !css.prevInput.Light) ||
+				(inputs[css.PeerID].Pause && !css.prevInput.Pause)){ 
+				//Probably grab the button type from the button instead?
+				EndgameVotes[css.PeerID] = (EndgameVote)(2 + css.x);
+				UnityEngine.Debug.Log(EndgameVotes[css.PeerID] );
+			}
+			css.prevInput = inputs[css.PeerID];
+		}
+		GoToNextScreen();
+
+	}
+
+	void SetUpVote() {
+		EndgameVotes = new EndgameVote[CharacterMenu.charSelStates.Length];
+
+		foreach(CharacterSelectState css in CharacterMenu.charSelStates){
+			if(css != null && css.PeerID < 8){
+				css.Cursor =  GameController.Instance.CreatePrefab("CharacterCursor",buttons[0].transform.position, transform.rotation);
+				css.x = 0;
+				css.Cursor.GetComponent<SpriteRenderer>().color = CharacterMenu.ColorFromEnum[(PlayerColor)css.PeerID];
+				EndgameVotes[css.PeerID] = EndgameVote.NO_SELECTION;
+			}
+		}
+	}
+
+	void GoToNextScreen(){
+		bool returnToCharacterSelect = false;
+		foreach(EndgameVote vote in EndgameVotes){
+			switch(vote){
+				case EndgameVote.NO_SELECTION:
+				//A player has not made a selection yet, so no action will be taken yet
+					return;
+				case EndgameVote.DISCONNECT:
+				//Disconnect a player from the game who is done playing, then set their slot to empty when theyre gone
+					backToMain(); //Replace this with actual DC code
+					return;
+					//break;
+				case EndgameVote.CHARACTER_SELECT:
+				//A player wants to change characters or stages
+					returnToCharacterSelect = true;
+					break;
+				case EndgameVote.PLAY_AGAIN:
+				//Player is ready to play again
+				case EndgameVote.EMPTY_SLOT:
+				//Ignore empty CSS slots
+				default:
+					break;
+			}
 		}
 
+		if(returnToCharacterSelect) GameController.Instance.GoToCharacterSelect();
+		else GameController.Instance.BeginMatch();
+		EndgameVotes = null;
 	}
 
 	public void setWinnerPic(DrifterType type,Color color) {
@@ -63,25 +134,62 @@ public class EndScreenManager : UIMenuManager {
 			}
 		}
 	}
-	public void backToMain() {
-		//Disconnect associated peer immediately
+	void DisconnectPlayerFromGame(int PeerID){
+		//Disconnect user from GGPO game here
+		GameController.Instance.removeUserByPeer(PeerID);
 		GameController.Instance.GoToMainMenu();
 	}
 
-	public void playAgain() {
-		//Vote to play again with curetn characters here
-		GameController.Instance.BeginMatch();
-		//NOTE: used to have some network cleanup and start client calls here (see mouse removal revision for commented out code if necessary)
-	}
-
-	public void returnToCharacterSelect() {
-		//Return to character select with all players who stayed
-		GameController.Instance.GoToCharacterSelect();
-		//NOTE: used to have some network cleanup and start client calls here (see mouse removal revision for commented out code if necessary)
+	public void backToMain() {
+		//Disconnect associated peer immediately
+		EndgameVotes = null;
+		GameController.Instance.GoToMainMenu();
 	}
 
 	public override void Exit() {
 		//Ragequit button lmao
 		GameController.Instance.GoToMainMenu();
+	}
+
+	public void Serialize(BinaryWriter bw) {
+
+		// bw.Write(countingPrevScreen);
+		// bw.Write(prevScreenTimer);
+		// bw.Write((int)phase);
+
+		// if(EndgameVotes != null){
+		// 	bw.Write(true);
+		// 	foreach(EndgameVote vote in EndgameVotes)
+		// 		bw.Write((int)vote);
+		// }
+		// else
+		// 	bw.Write(false);
+		
+		
+		// for(int i = 0; i < CharacterMenu.charSelStates.Length; i++){
+		// 	if(CharacterMenu.charSelStates[i] !=  null){
+		// 		bw.Write(true);
+		// 		CharacterMenu.charSelStates[i].Serialize(bw);
+		// 	}
+		// }
+	}
+
+	public void Deserialize(BinaryReader br) {
+
+		// // countingPrevScreen = br.ReadBoolean();
+		// // prevScreenTimer = br.ReadInt32();
+		// // phase = (CharacterMenuState)br.ReadInt32();
+		// if(br.ReadBoolean()){
+
+		// }
+
+	
+		// for(int i = 0; i < CharacterMenu.charSelStates.Length; i++){
+		// 	if(br.ReadBoolean()){
+		// 		if(CharacterMenu.charSelStates[i] ==  null) CharacterMenu.Instance.AddCharSelState(i,DrifterType.None);
+		// 		CharacterMenu.charSelStates[i].Deserialize(br);
+		// 	}
+		// 	else if(CharacterMenu.charSelStates[i] !=  null) CharacterMenu.Instance.RemoveCharSelState(i);
+		// }
 	}
 }
