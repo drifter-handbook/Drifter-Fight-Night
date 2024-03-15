63209452316c47a597481c38435c8efb231243bc
Cindy Prentice <prentlikes2cookies@gmail.com>
2024-03-15 18:08:22 -0500

    Scroll menu improvements (closer to being not broken)

--- Assets/Scripts/UI/SimpleScroll.cs
+++ Assets/Scripts/UI/SimpleScroll.cs
@@ -14,34 +14,35 @@ public class SimpleScroll : MonoBehaviour {
     void Update() {
         if(uiMenuManager.menuFlowHistory[uiMenuManager.menuFlowHistory.Count - 1] == UIMenuType.RebindMenu) {
             GameObject currentGameObject = EventSystem.current.currentSelectedGameObject;
-            if(currentGameObject!= null && currentGameObject.name != "Back Settings") {
-                SnapTo(EventSystem.current.currentSelectedGameObject);
+            if(currentGameObject!= null) {
+                SnapTo();
             }
         }
     }
 
-    public void SnapTo(GameObject target) {
-        int index = listItems.IndexOf(target); //Inventory Children List contains all the Children of the ScrollView's Content. We are getting the index of the selected one.
+    public void SnapTo() {
+        int index = listItems.IndexOf(EventSystem.current.currentSelectedGameObject); //Inventory Children List contains all the Children of the ScrollView's Content. We are getting the index of the selected one.
        
         if(index < 0) {
-            UnityEngine.Debug.LogWarning("Could not find the element to SnapTo");
+            Debug.LogWarning("Could not find the element to SnapTo");
             return;
         }  
         
-        RectTransform rect = listItems[index].GetComponent<RectTransform>(); //We are getting the RectTransform of the selected Inventory Item.
-        bool inView = RectTransformUtility.RectangleContainsScreenPoint(scrollRect, rect.position); //We are checking if the Selected Inventory Item is visible from the camera.
-        float buttonHeight = rect.rect.height;
+        GameObject rect = EventSystem.current.currentSelectedGameObject; //We are getting the RectTransform of the selected Inventory Item.
+        float viewing_top_y = scrollRect.position.y;
+        float viewing_bottom_y = scrollRect.position.y + (scrollRect.rect.height);
+        bool inView = rect.transform.position.y > viewing_top_y && rect.transform.position.y < viewing_bottom_y;
+        float buttonHeight = rect.GetComponent<RectTransform>().rect.height;
 
         if (!inView) { //If the selected Item is not visible.
-            if (oldRect != null) { //If we haven't assigned Old before we do nothing.
-                if (oldRect.localPosition.y < rect.localPosition.y) { //If the last rect we were selecting is lower than our newly selected rect.
-                    contentPanel.anchoredPosition += new Vector2(0, -buttonHeight); //We move the content panel down.
+                if (rect.transform.position.y < viewing_top_y) { //If the last rect we were selecting is lower than our newly selected rect.
+                    Debug.Log("Moving content down. rect.position.y = " + rect.transform.position.y);
+                    contentPanel.anchoredPosition += new Vector2(0, buttonHeight); //We move the content panel down.
                 }
-                else if (oldRect.localPosition.y > rect.localPosition.y) { //if the last rect we were selecting is higher than our newly selected rect.
-                    contentPanel.anchoredPosition += new Vector2(0, buttonHeight); //We move the content panel up.
+                else if (rect.transform.position.y > viewing_bottom_y) { //if the last rect we were selecting is higher than our newly selected rect.
+                    Debug.Log("Moving content up. rect.position.y = " + rect.transform.position.y);
+                    contentPanel.anchoredPosition += new Vector2(0, -buttonHeight); //We move the content panel up.
                 }
-            }
         }
-        oldRect = rect; //We assign our newly selected rect as the OldRect.
     }
 }
